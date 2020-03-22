ea7cee50b853472eb9415a8d4e2f8d4720624314
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 13:59:16 +0530

    code added

---
+++ AdvanceWebApi3/Controllers/VersioningController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvanceWebApi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersioningController : ControllerBase
+    {
+
+        //Implement Versioning using three different approaches.
+    }
+}
